692c6521f02097eca6c14c86ef700681ba2e1816
kevinthink <kevin2android@gmail.com>
2016-09-20 09:20:38 +0800

    小修改

--- ex2/app.js
+++ ex2/app.js
@@ -1,5 +1,3 @@
-
-
 $(function () {
 
     var $btn1 = $("#btn1");
@@ -8,23 +6,24 @@ $(function () {
     var $input3 = $("#input3");
     var $input4 = $("#input4");
 
-    $btn1.click(function () {
+    // $btn1 滑鼠點擊時，執行 changeText 函數(動作)
 
-        //        $("div").text("AAA");
+    $btn1.click(function () {
 
         changeText();
 
-
     });
 
+    // changeText 函數(動作)
     function changeText() {
 
+        // 將表單輸入文字 指定給變數
         var title1 = $input1.val();
         var title2 = $input2.val();
         var title3 = $input3.val();
         var title4 = $input4.val();
 
-
+        // 將變數指定給文字區塊
         $("#text1").text(title1);
         $("#text2").text(title2);
         $("#text3").text(title3);
